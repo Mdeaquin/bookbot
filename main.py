4acb5611709aa853ce0481dec974fa875064942d
Mdeaquin <mdeaquin21@gmail.com>
2025-08-05 19:58:10 -0500

    feat : return book word count

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = get_book_words(text)
+    print(f"{count} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_book_words(book_string):
+    word_count = 0
+    words = book_string.split()
+    for word in words:
+        word_count += 1
+    return word_count
 
 main()
